efbabd7e1376e9426c43efd677acf0949ebe7d07
evagi <eva.gilyazova2003@gmail.com>
2023-03-04 02:22:48 +0300

    init

--- typelayout/src/main/java/ru/mirea/gilyazoivaer/typelayout/MainActivity.java
+++ typelayout/src/main/java/ru/mirea/gilyazoivaer/typelayout/MainActivity.java
@@ -3,12 +3,30 @@ package ru.mirea.gilyazoivaer.typelayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView tvMirea;
+private Button btnChangeMireaText;
+private int count=0;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+   /* protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvMirea=findViewById(R.id.textViewMirea);
+        btnChangeMireaText=findViewById((R.id.buttonChangeMireaText));
+        View.OnClickListener onClickListener=new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvMirea.setText(String.format("Click number: %d",count++));
+            }
+        };
+        btnChangeMireaText.setOnClickListener(onClickListener);
+    }*/
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
     }
 }
